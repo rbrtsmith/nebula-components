e15353b06dd3ca2ff8e4a0b04f622d80ac033382
Kay Wood <kwood15@users.noreply.github.com>
2017-09-20 11:05:38 +0100

    Remove redundant code

--- demo/src/Components/ProgressBar/index.jsx
+++ demo/src/Components/ProgressBar/index.jsx
@@ -4,23 +4,17 @@ const ProgressBar = () => (
   <div>
     <section className="o-section-md">
       <div className="c-progress-bar">
-        <div className="c-progress-bar__indicator c-progress-bar__indicator--current">
-          Current
-        </div>
+        <div className="c-progress-bar__indicator c-progress-bar__indicator--current" />
       </div>
     </section>
     <section className="o-section-md">
       <div className="c-progress-bar">
-        <div className="c-progress-bar__indicator c-progress-bar__indicator--success">
-          Success
-        </div>
+        <div className="c-progress-bar__indicator c-progress-bar__indicator--success" />
       </div>
     </section>
     <section className="o-section-md">
       <div className="c-progress-bar">
-        <div className="c-progress-bar__indicator c-progress-bar__indicator--error">
-          Error
-        </div>
+        <div className="c-progress-bar__indicator c-progress-bar__indicator--error" />
       </div>
     </section>
   </div>
